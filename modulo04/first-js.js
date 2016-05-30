33036433f7dba7fdd0ad41ec6c16146e7b9dc2a1
Simone Taeggi <startae14@gmail.com>
2016-05-30 19:29:05 +0200

    begin js

---
+++ modulo04/first-js.js
@@ -0,0 +1,20 @@
+var firstNumber = 1;
+var secondNumber = 2;
+
+firstNumber += secondNumber;
+
+firstNumber = 2;
+
+firstNumber == 2;
+
+firstNumber = 1;
+
+var thirdNumber = firstNumber + secondNumber;
+
+thirdNumber += 1;
+
+thirdNumber -= 2;
+
+window.alert("Hello");
+
+document.write('<h1>Header</h1><p>Test Text to the body</p><footer><small>Footer</small></footer>');
