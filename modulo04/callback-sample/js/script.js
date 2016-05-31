3d5febe7336024302341476bcf67407988ac342e
Simone Taeggi <startae14@gmail.com>
2016-05-31 23:38:59 +0200

    callback

--- modulo04/callback-sample/js/script.js
+++ modulo04/callback-sample/js/script.js
@@ -7,12 +7,15 @@ che prende in input tre parametri:
  - una value per il cookie
  - è un qualcoa che vogliamo accada quando abbiamo finito
 */
-function saveToCookie(){
+function saveToCookie(key, value, onCompletion){
    // assembliamo il cookie semplicemente concatenando la key, value
    var cookieValue = key + ',' + value;
    
    // ora assegniamo al cookie del document
-   document.cookie += cookieValue;    
+   document.cookie += cookieValue;
+   
+   // ed invoco la terza funzione
+   onCompletion(cookieValue);    
 }
 
 function saveTextValue(){
@@ -24,10 +27,25 @@ function saveTextValue(){
 
 function showTextSuccess(result){
     // faccio un alert con il contenuto di result
-    window.alert('You successfully saved [' + result ']')
+    window.alert('You successfully saved [' + result + '] from the text input to your cookie.');
     
 }
 
 function saveRadioValue(){
+    var radioValue;
+    var radioOptions = document.getElementsByName('department');
     
+    // creo un for per ciclare tutti i valori
+    for (var index = 0; index < radioOptions.length; index++) {
+        // controlliamo se l'elemento è checkato
+        if(radioOptions[index].checked) {
+            // assegno a radioValue il valore dell'elemento
+            radioValue = radioOptions[index].value;
+            break;
+        }
+    }
+    // invochiamo ora il saveToCookie
+    saveToCookie('Department', radioValue, function (result) {
+       window.alert("You did it! You saved [" + result + "]"); 
+    });
 }
